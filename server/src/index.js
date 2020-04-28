99c8af942cdb6187082961c31801a6b7fec6f4cc
terodox <terodox@gmail.com>
2020-04-28 19:39:51 -0400

    Setup api for json and rest responses

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,16 @@
 const Koa = require('koa');
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    await next();
+  });
+
 app.use(async ctx => {
-  ctx.body = 'Hello World';
-  ctx.header['Access-Control-Allow-Origin'] = '*';
+    ctx.type = 'json';
+    ctx.body = { message: 'Hello World' };
 });
 
 const port = process.env.PORT || 5000;
