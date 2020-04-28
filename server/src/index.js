825b804e8c7343c188f0c2e0204e5190dc5ba5cc
terodox <terodox@gmail.com>
2020-04-28 19:23:19 -0400

    Trying again

--- server/src/index.js
+++ server/src/index.js
@@ -5,4 +5,7 @@ app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.listen(5000);
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`App is listening on ${port}`);
