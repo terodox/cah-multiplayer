98368c59032a4f7480a5c057400ca6c92a4802bc
terodox <terodox@gmail.com>
2020-04-28 19:39:58 -0400

    Setup api for json and rest responses

--- server/src/index.js
+++ server/src/index.js
@@ -5,11 +5,11 @@ app.use(async (ctx, next) => {
     ctx.set('Access-Control-Allow-Origin', '*');
     ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    ctx.type = 'json';
     await next();
   });
 
 app.use(async ctx => {
-    ctx.type = 'json';
     ctx.body = { message: 'Hello World' };
 });
 
