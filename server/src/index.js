b697856fbe4736c4d5dcae9efe9a4d448f8c42ba
terodox <terodox@gmail.com>
2020-05-02 11:11:38 -0400

    Setup card selection endpoint

--- server/src/index.js
+++ server/src/index.js
@@ -28,6 +28,7 @@ app.use(route.get('/cards', require('./cards')));
 app.use(route.get('/cards/black/random', require('./cards-black-random')));
 app.use(route.get('/games/:gameid', require('./games-get')));
 app.use(route.get('/games/:gameid/players/:playerid', require('./players-get')));
+app.use(route.patch('/games/:gameid/players/:playerid', require('./players-patch')));
 app.use(route.patch('/games/:gameid', require('./games-patch')));
 
 const port = process.env.PORT || 80;
---
+++ server/src/players-patch.js
@@ -0,0 +1,69 @@
+const Game = require('./models/game');
+const Player = require('./models/player');
+
+module.exports = async function playersGet(ctx, gameId, playerId) {
+    const collection = ctx.mongo.db('cah-multiplayer').collection('games');
+
+    let player;
+    try {
+        console.log('PlayerId:', playerId);
+        player = new Player({
+            name: playerId
+        });
+    } catch(error) {
+        console.error(error);
+        ctx.status = 400;
+        ctx.body = { errorMessage: error.message };
+        return;
+    }
+
+    try {
+        let result = await collection.find({ name: gameId }).toArray();
+        console.log('Get result', result);
+
+        if(result.length === 0) {
+            ctx.status = 404;
+            return;
+        }
+
+        let patchBody = ctx.request.body;
+        if(typeof ctx.request.body === 'string') {
+            patchBody = JSON.parse(ctx.request.body);
+        }
+
+        // TODO validate card is in hand
+
+        if(!patchBody.selectedCard) {
+            ctx.status = 204;
+            return;
+        }
+
+        const gameFromDb = result[0];
+        const game = new Game(gameFromDb);
+        const foundPlayer = game.players.find(gamePlayer => gamePlayer.name === player.name);
+        const foundPlayerIndex = game.players.indexOf(foundPlayer);
+        console.log('foundPlayerIndex', foundPlayer);
+        if(foundPlayer) {
+            const updatedPlayer = new Player({
+                ...foundPlayer,
+                patchBody
+            });
+
+            await collection.updateOne({
+                _id: gameFromDb._id,
+            }, {
+                $set: {
+                    [`players.${foundPlayerIndex}`]: updatedPlayer
+                }
+            })
+        } else {
+            ctx.status = 404;
+            return;
+        }
+        ctx.status = 204;
+    } catch(error) {
+        console.log(error);
+        ctx.status = 500;
+        //This is a comment
+    }
+}
